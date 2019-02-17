d865bedafd5f45e551866b760c4ee6282e33744b
sagarninave <sagarninave@gmail.com>
2019-02-17 16:34:39 +0530

    Update views.py
    registration index has been update

--- registration/views.py
+++ registration/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<h1> hello </h1>")
+	return HttpResponse("<h1 align=center> Hello User </br> Please go to <strong> https://127.0.0.1:8000/home </strong>  for home page </h1>")
